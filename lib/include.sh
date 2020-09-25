3b795c27efcb7858c2f8136d30c883463bbe1a1a
Walter White <WalterWhite@walterjwhite.com>
2020-09-25 16:52:03 -0400

    merged

---
+++ lib/include.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# ensure we're in the expenses directory
+_FIELDS="name,description,category,comments,date"
+_COST_FIELDS="unit-price,quantity,tax,tip,total"
+
+_EXPENSES_FILE=.expenses
